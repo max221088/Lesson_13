764fd1f599596d8f540d2a3d515687f477b13025
max221088 <max.work221088@gmail.com>
2022-12-11 18:10:19 +0200

    finished modal

--- js/index.js
+++ js/index.js
@@ -2,20 +2,26 @@ const $dropBtn = document.querySelector(".drop-btn");
 const $dropMenu = document.querySelector(".drop-menu");
 const $table = document.querySelector("tbody");
 const $filt = document.querySelector(".input-search");
+const $modal = document.querySelector(".modal-container");
 let tableHtml = "";
 let pokemon = [];
 let filtPokemon = [];
+let url = [];
 
 $dropBtn.addEventListener("click", function () {
 	$dropMenu.classList.toggle("active");
 });
 
 function htmlgenerator(data) {
+	//console.log(data);
 	if (data) {
 		tableHtml +=
 			"<tr>" +
 			"<td><input type =checkbox class= checkbox></td>" +
-			"<td> <img class=front-pokemon src=" +
+			"<td> <img data-name=" +
+			data.name +
+			" " +
+			"class=front-pokemon src=" +
 			data.sprites.front_default +
 			"></td>" +
 			"<td>" +
@@ -85,43 +91,70 @@ fetch("https://pokeapi.co/api/v2/pokemon/")
 		contentGenerator(pokemon);
 	});
 
+function openModal(data) {
+	if (data == open) {
+		$modal.classList.add("active");
+	} else if (data == close) {
+		$modal.classList.remove("active");
+	}
+}
+
 $table.addEventListener("click", function (event) {
-	let url = event.target.dataset.url;
-	if (url) {
-		fetch(event.target.dataset.url)
+	console.log(event);
+	let query = event.target.dataset.name;
+	if (query) {
+		fetch("https://pokeapi.co/api/v2/pokemon/")
 			.then(function (response) {
 				return response.json();
 			})
 			.then(function (data) {
-				let modal =
-					"<h3 class =modal-tittle>" +
-					data.name +
-					"</h3>" +
-					"<div class = front-default>" +
-					"<img src = " +
-					data.sprites.other.dream_world.front_default +
-					">" +
-					"</div>" +
-					"<div class = foot-bar>" +
-					"<img src = " +
-					data.sprites.back_default +
-					">" +
-					"<img src = " +
-					data.sprites.back_shiny +
-					">" +
-					"<img src = " +
-					data.sprites.front_default +
-					">" +
-					"<img src = " +
-					data.sprites.front_shiny +
-					">" +
-					"</div>";
-				document.querySelector(".modal").innerHTML = modal;
-				console.log(data);
+				//console.log(data.results);
+				url = data.results.filter(function (el) {
+					if (el.name.indexOf(query) != -1) {
+						return true;
+					} else {
+						return false;
+					}
+				});
+				url = url[0].url;
+				console.log(url);
+				fetch(url)
+					.then(function (response) {
+						return response.json();
+					})
+					.then(function (data) {
+						console.log(data);
+						let modal =
+							"<h3 class =modal-tittle>" +
+							data.name +
+							"</h3>" +
+							"<div class = front-default>" +
+							"<img src = " +
+							data.sprites.other.dream_world.front_default +
+							">" +
+							"</div>" +
+							"<div class = foot-bar>" +
+							"<img src = " +
+							data.sprites.back_default +
+							">" +
+							"<img src = " +
+							data.sprites.back_shiny +
+							">" +
+							"<img src = " +
+							data.sprites.front_default +
+							">" +
+							"<img src = " +
+							data.sprites.front_shiny +
+							">" +
+							"</div>";
+						document.querySelector(".modal").innerHTML = modal;
+						console.log(data);
+					});
 			});
 		openModal(open);
 	}
 });
+
 document.querySelector(".overlay").addEventListener("click", function () {
 	openModal(close);
 });
